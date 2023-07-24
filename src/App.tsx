af02cb3706f7cd3ddc49fd02f5708a2c98524b2e
Juris Reisons <j.reisons@gmail.com>
2023-07-24 17:58:13 +0200

    Layout tweak

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
 
   const [inputData, setInputData] = useState<InputData>(
     {
-      cashflowString: 'zeros(1,10)',
+      cashflowString: 'zeros(10)',
       cashflowStringValid: true,
       cashflows: [0,0,0,0,0,0,0,0,0,0,0],
     });
