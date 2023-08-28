e9a5c4d7f71e50f9a3aa39e6f1c7763ccea21cbc
Juris Reisons <j.reisons@gmail.com>
2023-08-28 09:43:41 +0200

    Remove wealth grid form

--- src/input/main.tsx
+++ src/input/main.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { CashflowsForm, CashflowsFormState } from './cashflows';
 import { StrategiesForm, StrategiesFormState } from './strategies';
 import { UtilityForm, UtilityFormState } from './utility';
-import { WealthGridForm, WealthGridFormState } from './wealthGrid';
 
 
 export interface MainFormState {
     cashflowsFormState: CashflowsFormState;
     strategiesFormState: StrategiesFormState;
-    wealthGridFormState: WealthGridFormState;
     utilityFormState: UtilityFormState;
 }
 
@@ -22,15 +20,12 @@ export const MainForm: React.FC<MainFormProps> = ({ state, setState }) => {
     const setCashflows = (cashflowsFormState: CashflowsFormState) => { setState({ ...state, cashflowsFormState: cashflowsFormState }) };
     const setStrategies = (strategiesFormState: StrategiesFormState) => { setState({ ...state, strategiesFormState: strategiesFormState }) };
     const setUtility = (utilityFormState: UtilityFormState) => { setState({ ...state, utilityFormState: utilityFormState }) };
-    const setWealthGrid = (wealthGridFormState: WealthGridFormState) => { setState({ ...state, wealthGridFormState: wealthGridFormState }) };
 
     return (
         <div className="top-container">
             <CashflowsForm state={state.cashflowsFormState} setState={setCashflows} />
 
             <StrategiesForm state={state.strategiesFormState} setState={setStrategies} />
-
-            <WealthGridForm state={state.wealthGridFormState} setState={setWealthGrid} />
 
             <UtilityForm state={state.utilityFormState} setState={setUtility} />
         </div>
