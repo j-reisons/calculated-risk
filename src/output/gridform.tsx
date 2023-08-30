907a58b614c0235c3e9caca67d590051492e4f19
Juris Reisons <j.reisons@gmail.com>
2023-08-30 09:22:12 +0200

    Grid params validation

--- src/output/gridform.tsx
+++ src/output/gridform.tsx
@@ -19,11 +19,11 @@ export const GridForm = ({ state, setState }: GridFormProps) => {
 
     // TODO: less insane validation
     const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value: number = parseInt(event.target.value);
+        const value: number | null = parseIfValid(event);
         setState({
             ...state,
             [event.target.id]: event.target.value,
-            gridSize: {
+            gridSize: !value ? state.gridSize : {
                 ...state.gridSize,
                 [event.target.id]: value
             }
@@ -32,10 +32,28 @@ export const GridForm = ({ state, setState }: GridFormProps) => {
 
     return (
         <div>
-            <div>Wealth max <input type="text" inputMode="numeric" value={state.wealthMax} onChange={handleInput} id="wealthMax" name="wealthMax" pattern="^-?\d+$" /></div>
-            <div>Wealth step <input type="text" inputMode="numeric" value={state.wealthStep} onChange={handleInput} id="wealthStep" name="wealthStep" pattern="^-?\d+$" /></div>
+            <div>Wealth max <input type="text" inputMode="numeric" value={state.wealthMax} onChange={handleInput} id="wealthMax" name="wealthMax" pattern="^\d+$" /></div>
+            <div>Wealth step <input type="text" inputMode="numeric" value={state.wealthStep} onChange={handleInput} id="wealthStep" name="wealthStep" pattern="^\d+$" /></div>
             <div>Wealth min <input type="text" inputMode="numeric" value={state.wealthMin} onChange={handleInput} id="wealthMin" name="wealthMin" pattern="^-?\d+$" /></div>
-            <div>Periods <input type="text" inputMode="numeric" value={state.periods} onChange={handleInput} id="periods" name="periods" pattern="^-?\d+$" /></div>
+            <div>Periods <input type="text" inputMode="numeric" value={state.periods} onChange={handleInput} id="periods" name="periods" pattern="^\d+$" /></div>
         </div>
     )
+}
+
+function parseIfValid(event: React.ChangeEvent<HTMLInputElement>): number | null {
+    let value = null;
+    try {
+        value = parseInt(event.target.value);
+    }
+    catch (e) {
+        return null;
+    }
+
+    switch (event.target.id) {
+        case "wealthMin":
+            break;
+        default:
+            if (value <= 0) return null;
+    }
+    return value;
 }
