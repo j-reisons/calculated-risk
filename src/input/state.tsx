eb1da3fd775b2b642b0176f31a54bbbe5493827f
Juris Reisons <j.reisons@gmail.com>
2023-10-11 18:56:28 +0200

    Cache erf computation

--- src/input/state.tsx
+++ src/input/state.tsx
@@ -38,7 +38,23 @@ export interface Strategy_ {
 }
 
 export function normalCdf(mu: number, sigma: number): (r: number) => number {
-    return (r: number) => { return 0.5 * (1 + erf((r - mu) / (1.41421356237 * sigma))) }
+    return (r: number) => { return 0.5 * (1 + cachedErf((r - mu) / (1.41421356237 * sigma))) }
+}
+
+const CACHE = Array<number>(20001);
+const START = -5;
+const END = 5;
+const STEP = ((END - START) / (CACHE.length - 1));
+for (let i = 0; i < CACHE.length; i++) {
+    CACHE[i] = erf(START + STEP * i);
+}
+
+function cachedErf(x: number): number {
+    if (x < START + STEP) return -1;
+    if (x > END - STEP) return 1;
+    const i = Math.floor((x - START) / STEP);
+    const rest = x - START - (STEP * i);
+    return (1 - rest) * CACHE[i] + (rest) * CACHE[i + 1];
 }
 
 export interface UtilityFormState {
