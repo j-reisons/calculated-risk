942324692924e1ce730a63c33807ec232b2135c7
Juris Reisons <j.reisons@gmail.com>
2023-11-23 12:52:25 +0100

    Rework comment

--- src/solver/core.ts
+++ src/solver/core.ts
@@ -3,11 +3,15 @@ import { NdArray } from "ndarray";
 // A tensor of dimensions (periods, starting_wealth, strategy, next_wealth)
 // Contains transition probabilities from starting_wealth to next_wealth 
 // for a given period and strategy
-// Two properties of the tensor are exploited to reduce memory footprint
+// Two properties of the tensor are exploited to reduce memory footprint and speed up calculations.
 // * Period slices of the tensor only differ if the period cashflows differ. 
 //   Only unique slices are stored.
-// * The tensor has a band structure in the (starting_wealth, next_wealth) indices
-//   Only non-zero elements are stored, along with band locations and widths.
+// * The tensor has a rough band structure in the (starting_wealth, next_wealth) indices.
+//   More specifically for any (periods,starting_wealth,strategy) slice there is a single 
+//   next_wealth interval over which the transition tensor can take non-zero values.
+//   Locations and widths of these intervals are stored, and values are stored 
+//   in an NdArray sized (starting_wealth, strategy, max_bandwidth) instead of (starting_wealth, strategy, next_wealth).
+//   This could be further compacted to store only non-zero values ( as-is bands narrower than max_bandwidth include zeroes ) but I haven't felt the need yet yet.
 export interface TransitionTensor {
     // [unique_periods](starting_wealth, strategy, max_bandwidth)
     values: NdArray[];
